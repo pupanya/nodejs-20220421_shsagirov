d94750482675c577969ae23a984e92fc890edd4d
pupanya <pupanya@mail.ru>
2022-05-24 01:34:35 +0500

    3 module 1 task realized transform function

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,15 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.used = 0;
+    this.limitSize = options.limit;
   }
 
   _transform(chunk, encoding, callback) {
+    this.used += Buffer.byteLength(chunk);
+    const err = this.used> this.limitSize ? new LimitExceededError() : null;
 
+    callback(err, chunk);
   }
 }
 
